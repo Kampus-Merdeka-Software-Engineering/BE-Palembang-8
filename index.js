8643d9e2e6459c070dc5196c4a4fc22b5dd0c207
AlrivaldiSyahrizalRusli <142799984+AlrivaldiSyahrizalRusli@users.noreply.github.com>
2023-10-09 23:32:51 +0700

    coba untuk bisa railway

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const router = require("./routes/router.js");
 const cors = require('cors');
+require('dotenv').config();
+const path = require('path');
+const connection = require('./models/index')
 
 app.use(cors({
   origin: "*", // Ini akan mengizinkan seluruh domain
@@ -12,9 +15,23 @@ app.use(cors({
 app.use(express.urlencoded({ extended: true }));
 app.use("/", router);
 
-app.listen(5000, function () {
-  console.log(`Server running on http://localhost:5000`);
-});
+// app.listen(5000, function () {
+//   console.log(`Server running on http://localhost:5000`);
+// });
+
+//baru
+const port = process.env.PORT || 5000;
+app.listen(port, "0.0.0.0", function(){
+    console.log("server start on", port)
+    connection.authenticate()
+    .then(function(){
+        console.log("Database terhubung")
+    })
+    .catch(function(err){
+        console.log("Error saat koneksi ke database", err)
+        process.exit()
+    })
+})
 
 
 
